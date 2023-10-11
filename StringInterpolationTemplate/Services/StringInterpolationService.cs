c603156b475b61e8b12e2adf248ef7ccef87e169
Julia Zblewski <jzblewski2@my.wctc.edu>
2023-10-10 22:00:22 -0500

    A7 commit

--- StringInterpolationTemplate/Services/StringInterpolationService.cs
+++ StringInterpolationTemplate/Services/StringInterpolationService.cs
@@ -25,56 +25,96 @@ public class StringInterpolationService : IStringInterpolationService
 
         return answer;
     }
-
+    //2. 2019.01.22
     public string Number02()
     {
-        throw new NotImplementedException();
-    }
+        var date = _date.Now.ToString("yyyy.MM.dd");
+        var answer = $"{date}";
+        Console.WriteLine(answer);
 
+        return answer;
+    }
+    //3. Day 22 of January, 2019
     public string Number03()
     {
-        throw new NotImplementedException();
-    }
+        var date = _date.Now;
+        var answer = $"Day {date:dd} of January, {date:yyyy}";
+        Console.WriteLine(answer);
 
+        return answer;
+    }
+    //4. Year: 2019, Month: 01, Day: 22
     public string Number04()
     {
-        throw new NotImplementedException();
-    }
+        var date = _date.Now;
+        var answer = ($"Year: {date:yyyy}, Month: {date:MM}, Day: {date:dd}");
+        Console.WriteLine(answer);
 
+        return answer;
+    }
+    //5. Tuesday(10 spaces total, right aligned)
     public string Number05()
     {
-        throw new NotImplementedException();
-    }
+        var date = _date.Now.ToString("dddd");
+        var answer = $"{date,10}";
+        Console.WriteLine(answer);
 
+        return answer;
+    }
+    //6. 11:01 PM Tuesday(10 spaces total for each including the day - of - week, both right - aligned)
     public string Number06()
     {
-        throw new NotImplementedException();
-    }
+        var date = _date.Now;
+        var answer = $"{date,10:t}{date,10:dddd}";
+        Console.WriteLine(answer);
 
+        return answer;
+    }
+    //7. h:11, m: 01, s: 27
     public string Number07()
     {
-        throw new NotImplementedException();
-    }
+        var date = _date.Now;
+        var answer = $"h:{date:hh}, m:{date:mm}, s:{date:ss}";
+        Console.WriteLine(answer);
 
+        return answer;
+
+    }
+    //8. 2019.01.22.11.01.27
     public string Number08()
     {
-        throw new NotImplementedException();
-    }
+        var date = _date.Now.ToString("yyyy.MM.dd.hh.mm.ss");
+        var answer = $"{date}";
+        Console.WriteLine(answer);
 
+        return answer;
+
+    }
+    //9. Output pi as currency
     public string Number09()
     {
-        throw new NotImplementedException();
-    }
+        double pi = Math.PI;
+        var answer = $"{pi:C}";
+        Console.WriteLine(answer);
 
+        return answer;
+    }
+    //10. Output as right - aligned(10 spaces), number with 3 decimal places
     public string Number10()
     {
-        throw new NotImplementedException();
+        double pi = Math.PI;
+        var answer = $"{pi,10:0.000}";
+        Console.WriteLine(answer);
+
+        return answer;
     }
 
     public string Number11()
     {
-        throw new NotImplementedException();
-    }
+        var date = _date.Now.ToString("yyyy");
+        var answer = $"{Convert.ToInt32(date):X3}";
+        Console.WriteLine(answer);
 
-    //2.2019.01.22
+        return answer;
+    }
 }
